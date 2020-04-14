7f15db8b27c5bf3aecfcb5cec639713979267ddd
myd10 <myd10@georgetown.edu>
2020-04-13 21:03:42 -0400

    testing enlarge

---
+++ my_script.py
@@ -0,0 +1,12 @@
+#testing-123.20/my_script.py
+
+def enlarge(i):
+    return i *100
+
+
+#need to remove from global scope for test
+
+if __name__ == "__main__":
+    my_number = float(input("Please input a number."))
+    n = enlarge(my_number)
+    print("Enlarging the number:", n)
